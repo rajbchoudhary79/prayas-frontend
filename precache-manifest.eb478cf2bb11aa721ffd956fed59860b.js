ba156711f2f50a91f346ffb90d3c7e96f7c6bdec
Rajendra Choudhary <rajbchoudhary79@gmail.com>
2022-03-06 09:39:14 +0530

    Updates

---
+++ precache-manifest.eb478cf2bb11aa721ffd956fed59860b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94bbe3225eebff897b4e",
+    "url": "/prayas-frontend/static/css/main.c3972d08.chunk.css"
+  },
+  {
+    "revision": "94bbe3225eebff897b4e",
+    "url": "/prayas-frontend/static/js/main.94bbe322.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/prayas-frontend/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "b75323fd636df612d81b",
+    "url": "/prayas-frontend/static/js/2.b75323fd.chunk.js"
+  },
+  {
+    "revision": "1814974fb67114335a06",
+    "url": "/prayas-frontend/static/js/runtime~main.1814974f.js"
+  },
+  {
+    "revision": "f6fa8e64020633acd504dc3b35417814",
+    "url": "/prayas-frontend/static/media/icon.f6fa8e64.png"
+  },
+  {
+    "revision": "712fe17ba79fefbeda5828c93394aa20",
+    "url": "/prayas-frontend/index.html"
+  }
+];
